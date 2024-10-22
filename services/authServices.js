0c6df38e719ac44e58a6a0416221143d458ba8bc
SharifuzzamanShaon <shasan2200@gmail.com>
2024-10-22 16:57:31 +0600

    async function modified

--- services/authServices.js
+++ services/authServices.js
@@ -6,27 +6,35 @@ const sendToken = require("../utils/token.jwt");
 
 // Roles & accountStatus using by admin function
 async function registerService({ name, email, password }) {
-  const isEmailExists = await findUserByProperty("email", email);
-  if (isEmailExists) throw error("User alreay exists", 400);
-  const hash = await bcrypt.hash(password, saltRounds);
-  return registerNewUser({
-    name,
-    email,
-    password: hash,
-  });
+  try {
+    const isEmailExists = await findUserByProperty("email", email);
+    if (isEmailExists) throw error("User alreay exists", 400);
+    const hash = await bcrypt.hash(password, saltRounds);
+    return registerNewUser({
+      name,
+      email,
+      password: hash,
+    });
+  } catch (error) {
+    console.log(error);
+  }
 }
 async function loginService({ email, password, res, next }) {
-  const validUser = await findUserByProperty("email", email);
-  if (!validUser) throw error("User not registered yet", 400);
+  try {
+    const validUser = await findUserByProperty("email", email);
+    if (!validUser) throw error("User not registered yet", 400);
 
-  const isValidPassword = await bcrypt.compare(password, validUser.password);
-  if (!isValidPassword) throw error("Invaid Password", 401);
-  const payload = {
-    _id: validUser._id,
-    username: validUser.username,
-    email: validUser.email,
-    role: validUser.role,
-  };
-  sendToken(payload, 200, res, next);
+    const isValidPassword = await bcrypt.compare(password, validUser.password);
+    if (!isValidPassword) throw error("Invaid Password", 401);
+    const payload = {
+      _id: validUser._id,
+      username: validUser.username,
+      email: validUser.email,
+      role: validUser.role,
+    };
+    sendToken(payload, 200, res, next);
+  } catch (error) {
+    console.log(error);
+  }
 }
 module.exports = { registerService, loginService };
--- services/user.js
+++ services/user.js
@@ -2,13 +2,17 @@ const User = require("../models/user.model");
 const error = require("../utils/error");
 
 function registerNewUser({ name, email, password }) {
-  const newUser = new User({
-    name,
-    email,
-    password,
-  });
+  try {
+    const newUser = new User({
+      name,
+      email,
+      password,
+    });
 
-  return newUser.save();
+    return newUser.save();
+  } catch (error) {
+    console.log(error);
+  }
 }
 
 const putUserById = async (id, data) => {
@@ -17,18 +21,30 @@ const putUserById = async (id, data) => {
   return User.findByIdAndUpdate(id, { ...data }, { new: true });
 };
 const findUserByProperty = (key, value) => {
-  if (key === "_id") {
-    return User.findById(value);
-  }
-  return User.findOne({ [key]: value }); //it means that the key of the object is dynamic and determined by the value of the key variable.
+  try {
+    if (key === "_id") {
+      return User.findById(value);
+    }
+    return User.findOne({ [key]: value });
+  } catch (error) {
+    console.log(error);
+  } //it means that the key of the object is dynamic and determined by the value of the key variable.
 };
 
 function getUsers() {
-  return User.find();
+  try {
+    return User.find();
+  } catch (error) {
+    console.log(error);
+  }
 }
 
 function deleteUser(key, value) {
-  return User.findOneAndRemove({ [key]: value });
+  try {
+    return User.findOneAndRemove({ [key]: value });
+  } catch (error) {
+    console.log(error);
+  }
 }
 
 module.exports = {

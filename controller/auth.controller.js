cbfad166b704fd7a6963bb7016cfd4b78675ecfd
SharifuzzamanShaon <shasan2200@gmail.com>
2024-10-22 17:48:20 +0600

    new login controller added

--- controller/auth.controller.js
+++ controller/auth.controller.js
@@ -2,6 +2,8 @@ const User = require("../models/user.model");
 const { registerService, loginService } = require("../services/authServices");
 const validateEmail = require("../utils/emailValidator");
 const { error } = require("../utils/error");
+const sendToken = require("../utils/token.jwt");
+const bcrypt = require("bcrypt");
 
 const registerController = async (req, res, next) => {
   try {
@@ -25,16 +27,44 @@ const registerController = async (req, res, next) => {
     next(error);
   }
 };
+// const loginController = async (req, res, next) => {
+//   try {
+//     const { email, password } = req.body;
+//     if (!email) throw error("email is required", 400);
+//     if (!password) throw error("password is required", 400);
+//     await loginService({ email, password, res, next });
+//   } catch (error) {
+//     next(error);
+//   }
+// };
+// ... existing code ...
+
 const loginController = async (req, res, next) => {
   try {
     const { email, password } = req.body;
-    if (!email) throw error("email is required", 400);
-    if (!password) throw error("password is required", 400);
-    await loginService({ email, password, res, next });
-  } catch (error) {
-    next(error);
+    if (!email || !password)
+      throw error("email and password are required", 400);
+
+    const user = await User.findOne({ email: email });
+    if (!user) throw error("user not found", 404);
+
+    const passwordMatch = await bcrypt.compare(password, user.password);
+    if (!passwordMatch) throw error("Password does not match", 401);
+
+    const payload = {
+      _id: user._id,
+      name: user.name,
+      email: user.email,
+      role: user.role,
+    };
+
+    sendToken(payload, 200, res, next);
+  } catch (err) {
+    next(err);
   }
 };
+
+// ... existing code ...
 const logout = async (req, res, next) => {
   try {
     res.cookie("access_token", "", { maxAge: 1 });

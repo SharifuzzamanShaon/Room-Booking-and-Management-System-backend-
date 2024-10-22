146c5ae50e4f5205a6b97967e27c07db936ba64d
SharifuzzamanShaon <shasan2200@gmail.com>
2024-10-22 14:14:10 +0600

    db confing with atlas

--- DB.config/connectDB.js
+++ DB.config/connectDB.js
@@ -1,5 +1,11 @@
-const { default: mongoose } = require("mongoose");
+const mongoose = require("mongoose");
 
 exports.connectDB = async () => {
-  await mongoose.connect(`${process.env.mongoDB_connection_str}`);
+  try {
+    await mongoose.connect(process.env.mongoDB_connection_str);
+    console.log("Successfully connected to MongoDB");
+  } catch (error) {
+    console.error("Error connecting to MongoDB:", error.message);
+    process.exit(1);
+  }
 };

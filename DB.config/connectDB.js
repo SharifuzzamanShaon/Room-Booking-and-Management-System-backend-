7d586b88c0c797f61f278f95a39020a4ef9ab20b
SharifuzzamanShaon <shasan2200@gmail.com>
2024-10-22 16:13:21 +0600

    Increase DB Connection Timeout

--- DB.config/connectDB.js
+++ DB.config/connectDB.js
@@ -2,7 +2,12 @@ const { default: mongoose } = require("mongoose");
 
 exports.connectDB = async () => {
   try {
-    await mongoose.connect(`${process.env.mongoDB_connection_str}`);
+    await mongoose.connect(`${process.env.mongoDB_connection_str}`, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+      serverSelectionTimeoutMS: 20000, // Increase timeout to 20 seconds
+      socketTimeoutMS: 45000, // Close sockets after 45 seconds of inactivity
+    });
   } catch (error) {
     console.log(error);
   }

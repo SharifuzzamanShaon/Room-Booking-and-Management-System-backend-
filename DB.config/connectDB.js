bb9c0f3691de33aed3f912ef83e7f7116880049d
SharifuzzamanShaon <shasan2200@gmail.com>
2024-10-22 14:25:39 +0600

    db config string

--- DB.config/connectDB.js
+++ DB.config/connectDB.js
@@ -2,7 +2,16 @@ const mongoose = require("mongoose");
 
 exports.connectDB = async () => {
   try {
-    await mongoose.connect(process.env.mongoDB_connection_str);
+    let connectionString = process.env.mongoDB_connection_str;
+    
+    if (!connectionString) {
+      throw new Error("MongoDB connection string is not defined in environment variables");
+    }
+
+    // Remove surrounding quotes if present (helpful for development environments)
+    connectionString = connectionString.replace(/^['"](.*)['"]$/, '$1');
+
+    await mongoose.connect(connectionString);
     console.log("Successfully connected to MongoDB");
   } catch (error) {
     console.error("Error connecting to MongoDB:", error.message);

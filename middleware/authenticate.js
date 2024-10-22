6313ec97df20614457fa9d436783d38f354004db
SharifuzzamanShaon <shasan2200@gmail.com>
2024-10-22 21:41:47 +0600

    modified the auth middleware for access token

--- middleware/authenticate.js
+++ middleware/authenticate.js
@@ -4,18 +4,25 @@ const User = require("../models/user.model");
 
 const authMiddleware = async (req, res, next) => {
   try {
-    const token = req.cookies.refresh_token;
-    const validToken = jwt.verify(token, process.env.REFRESH_TOKEN_KEY);
-    if (!validToken) throw error("invalid token", 400);
-    const userId = validToken._id; // Assuming the token contains the user's ID as 'id'
-    // Find the user in the database
-    const validUser = await User.findById({ _id: userId });
+    const token = req.cookies?.access_token;
 
-    if (!validUser) throw error("invalid user", 400);
-    if (!validUser)
-      return res
-        .status(400)
-        .send({ success: false, message: "Unauthenticate user" });
+    if (!token) {
+      throw error("No token provided", 401);
+    }
+
+    let validToken;
+    try {
+      validToken =jwt.verify(token, process.env.ACCESS_TOKEN_KEY);
+    } catch (jwtError) {
+      throw error("Invalid or expired token", 401);
+    }
+
+    const userId = validToken._id;
+    const validUser = await User.findById(userId);
+
+    if (!validUser) {
+      throw error("User not found", 401);
+    }
 
     req.user = validUser;
     next();
